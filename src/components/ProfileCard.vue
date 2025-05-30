d8a8b3aeacb32876cebda1298cc6415cdd65d207
unknown <saragihnindaaulia@gmail.com>
2025-05-30 23:47:37 +0700

    Update Profile Mahasiswa

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="profile-card">
+    <h1>Profil Mahasiswa</h1>
+    <ul>
+      <li><strong>Nama:</strong> Ninda Aulia Saragih</li>
+      <li><strong>NPM:</strong> 233510236</li>
+      <li><strong>Kelas:</strong> G</li>
+      <li><strong>Praktikum Kelas:</strong> D</li>
+      <li><strong>Asal:</strong> Rokan Hilir, Balai Jaya, Riau</li>
+    </ul>
+  </div>
+</template>
